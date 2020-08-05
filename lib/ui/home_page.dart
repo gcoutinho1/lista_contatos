5fdd082f51447a956687ea4dcff13eaf1adfe757
Guilherme Coutinho <guilhermecoutinho7777@gmail.com>
2020-08-05 17:24:22 -0300

    added options to call, edit and delete contact

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -29,7 +29,9 @@ class _HomePageState extends State<HomePage> {
       ),
       backgroundColor: Colors.orangeAccent,
       floatingActionButton: FloatingActionButton(
-        onPressed: () {_showContactPage();},
+        onPressed: () {
+          _showContactPage();
+        },
         child: Icon(Icons.add),
         backgroundColor: Colors.deepOrangeAccent,
       ),
@@ -73,13 +75,11 @@ class _HomePageState extends State<HomePage> {
                     ),
                     Text(
                       contacts[index].email ?? "",
-                      style: TextStyle(
-                          fontSize: 19),
+                      style: TextStyle(fontSize: 19),
                     ),
                     Text(
                       contacts[index].phone ?? "",
-                      style:
-                          TextStyle(fontSize: 19),
+                      style: TextStyle(fontSize: 19),
                     ),
                   ],
                 ),
@@ -88,27 +88,90 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-      onTap: (){
-        _showContactPage(contact: contacts[index]);
+      onTap: () {
+        _showOptions(context, index);
+//        _showContactPage(contact: contacts[index]);
       },
     );
   }
 
-  void _showContactPage({Contact contact}) async {
-    final recContact = await Navigator.push(context, MaterialPageRoute(builder: (context) => ContactPage(contact: contact,)));
-    if (recContact != null){
-      if(contact != null){
-        await helper.updateContact(recContact);
+  void _showOptions(BuildContext context, int index) {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return BottomSheet(
+            onClosing: () {},
+            builder: (context) {
+              return Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: FlatButton(
+                        child: Text(
+                          "Ligar",
+                          style: TextStyle(color: Colors.orange, fontSize: 20),
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: FlatButton(
+                        child: Text(
+                          "Editar",
+                          style: TextStyle(color: Colors.orange, fontSize: 20),
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                          _showContactPage(contact: contacts[index]);
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: FlatButton(
+                        child: Text(
+                          "Excluir",
+                          style: TextStyle(color: Colors.orange, fontSize: 20),
+                        ),
+                        onPressed: () {
+                          helper.deleteContact(contacts[index].id);
+                          setState(() {
+                            contacts.removeAt(index);
+                            Navigator.pop(context);
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        });
+  }
 
+  void _showContactPage({Contact contact}) async {
+    final recContact = await Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => ContactPage(
+                  contact: contact,
+                )));
+    if (recContact != null) {
+      if (contact != null) {
+        await helper.updateContact(recContact);
       } else {
         await helper.saveContact(recContact);
       }
       _getAllContacts();
     }
-
   }
 
-  void _getAllContacts(){
+  void _getAllContacts() {
     helper.getAllContacts().then((list) {
       setState(() {
         contacts = list;
